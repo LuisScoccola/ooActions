e196e86c5f3986742796fc908587d7390a534748
Luis Scoccola <luis.scoccola@gmail.com>
2016-06-19 22:04:48 -0300

    fixing style

--- ooAction2.v
+++ ooAction2.v
@@ -6,38 +6,39 @@ Import TrM.
 Context (G : ooGroup).
 Local Notation B := classifying_space.
 
+Section SomeLemmas.
 
-
-(* LEMMAS *)
   (* given a fibration [A : T -> U] and a dependent fibration [B (t : T) : (a : T a) -> U]
      we can form a product fibration [AxB t := { y : A t  &  B t y }].
      The lemma is that given a path [p] in [T] it is the same:
      [(transport AxB p x).1] and [transport A p x.1]
   *)
   Definition product_fibration
-             {T : Type} (A : T -> Type) (B : forall (t : T) (a : A t), Type) :
-               T -> Type
+             {T : Type}
+             (A : T -> Type) (B : forall (t : T) (a : A t), Type)
+             : T -> Type
   := fun t => { y : A t & B t y}.
   
-  Definition the_same
+  Definition transport_on_first_coordinate
              {T : Type} (A : T -> Type) (B : forall (t : T) (a : A t), Type)
-               {t1 t2 : T} (p : t1 = t2) (x : product_fibration A B t1) :
-                   (transport (product_fibration A B) p x).1 =
-                     transport A p x.1.
+             {t1 t2 : T} (p : t1 = t2) (x : product_fibration A B t1)
+             : (transport (product_fibration A B) p x).1 =
+                 transport A p x.1.
   Proof.
-    induction p.
-    reflexivity.
+    by induction p.
   Defined.
   
   (* a connected type is merely inhabited.
      I don't think this is done explicitly in the library
      although this argument is taken from the proof
      of [indecomposable_0connected] *)
-  Definition isinhabitedifisconnected (A : Type) : IsConnected 0 A -> merely A.
+  Definition isinhabitedifisconnected
+             (A : Type)
+             : IsConnected 0 A -> merely A.
   Proof.
     intro cc.
     assert (IsConnected -1 A) by refine (isconnected_pred -1 A).
-    assert (mA := center (merely A) : merely A).
+    pose (mA := center (merely A)).
     exact mA.
   Defined.
   
@@ -45,8 +46,8 @@ Local Notation B := classifying_space.
   (* a (forall) product of mere propositions is a mere proposition.
      could not find this in the library *)
   Definition ishprop_productofhprop `{Funext}
-               {A : Type} (P : A -> Type) (D : forall a, IsHProp (P a)) :
-                 IsHProp (forall a, P a).
+             {A : Type} (P : A -> Type) (D : forall a, IsHProp (P a))
+             : IsHProp (forall a, P a).
   Proof.
     refine (hprop_allpath _ _).
     intros f g.
@@ -57,16 +58,17 @@ Local Notation B := classifying_space.
 
   (* a (binary) product of mere propositions is a mere proposition. *)
    Definition ishprop_bi_productofhprop `{Funext}
-               {A B : Type} (pA : IsHProp A) (pB : IsHProp B) :
-                 IsHProp (A * B).
+              {A B : Type} (pA : IsHProp A) (pB : IsHProp B)
+              : IsHProp (A * B).
    Proof.
      refine (@trunc_prod (-1) A pA B pB ).
    Defined.
 
+End SomeLemmas.
 
-(* END LEMMAS *)
 
-(* DEFINITIONS *)
+Section BasicDefinitions.
+
   (* non-empty action *)
   Definition isNonempty (A : ooAction G) : Type
   := merely (action_space A).
@@ -98,13 +100,15 @@ Local Notation B := classifying_space.
   Definition isRegular' (A : ooAction G) : Type
   := Contr (sigT A).
   
-
+End BasicDefinitions.
   
 
-(* PROOFS *)   
+Section ProofsAboutDefinitions.
+
   (* a transitive action is non-empty *)
   Definition isnonempty_transitiveaction `{Univalence}
-               (A : ooAction G) : isTransitive A -> isNonempty A.
+             (A : ooAction G)
+             : isTransitive A -> isNonempty A.
   Proof.
     intro.
     unfold isNonempty. simpl.
@@ -113,9 +117,9 @@ Local Notation B := classifying_space.
     strip_truncations.
     cut (Trunc (-1) ((point (B G)) = mSA.1)).
     refine (Trunc_functor _ _).
-      intro p. exact (transport _ p^ (pr2 mSA)).
-    exact (merely_path_is0connected _ (point (B G)) (pr1 mSA)).
-    exact (isinhabitedifisconnected _ X).
+      - intro p. exact (transport _ p^ (pr2 mSA)).
+      - exact (merely_path_is0connected _ (point (B G)) (pr1 mSA)).
+      - exact (isinhabitedifisconnected _ X).
   Defined.
   
 
@@ -125,25 +129,29 @@ Local Notation B := classifying_space.
   
   (* being transitive is a mere property *)
   Definition ishProp_isTransitive `{Funext}
-               (A : ooAction G) : IsHProp (isTransitive A)
+             (A : ooAction G)
+             : IsHProp (isTransitive A)
   := ishprop_isconnected _ _.
   
   (* being transitive' is a mere property *)
   Definition ishProp_isTransitive' `{Funext}
-               (A : ooAction G) : IsHProp (isTransitive' A).
+             (A : ooAction G)
+             : IsHProp (isTransitive' A).
   Proof.
     unfold isTransitive'.
-    refine (ishprop_bi_productofhprop _ _).
+    exact (ishprop_bi_productofhprop _ _).
   Defined.
 
   
   Definition transitive2transitive' `{Univalence}
-               (A : ooAction G) : isTransitive A -> isTransitive' A.
+             (A : ooAction G)
+             : isTransitive A -> isTransitive' A.
   Proof.
     intro. unfold isTransitive'.
     refine ((isnonempty_transitiveaction A X) , _).
     intros x y.
-    pose (mereg := @merely_path_is0connected _ _ X ((point (B G)) ; x) ((point (B G)) ; y)).
+    pose (mereg := @merely_path_is0connected _ _ X ((point (B G)) ; x)
+                                                   ((point (B G)) ; y)).
     (* the following command takes some seconds *)
     simpl in mereg. simpl.
     cut (Trunc (-1) ((point (B G); x) = (point (B G); y))).
@@ -153,7 +161,8 @@ Local Notation B := classifying_space.
   Defined.
   
   Definition transitive'2transitive `{Univalence}
-               (A : ooAction G) : isTransitive' A -> isTransitive A.
+             (A : ooAction G)
+             : isTransitive' A -> isTransitive A.
   Proof.
     intro tr'. unfold isTransitive' in tr'.
     refine (is0connected_merely_allpath _ _).
@@ -181,18 +190,24 @@ Local Notation B := classifying_space.
         generalize txety''.
         refine (Trunc_rec _).
         intro txety'.
-        pose (txety := path_sigma _ ((point (B G)) ; tx) ((point (B G)) ; ty) (pr1 txety') (pr2 txety')).
+        pose (txety := path_sigma _ ((point (B G)) ; tx)
+                                    ((point (B G)) ; ty)
+                                    (pr1 txety') (pr2 txety')).
         (* finally compose the equalities *)
         exact (tr (xetx @ txety @ yety^)).
   Defined.
+
+End ProofsAboutDefinitions.
   
 
-(* MORE DEFINITIONS *)
+Section MoreDefinitions.
   (* orbit action: given an action [X] and a point [x] we can
      consider the orbit of this point. There is an
      action [orbit_action] that is the restriction of the action
      on [X] to the the orbit of [x] *)
-  Definition orbit_action (X : ooAction G) (x : action_space X) : ooAction G
+  Definition orbit_action
+             (X : ooAction G) (x : action_space X)
+             : ooAction G
   := fun b => { y : X(b) &
                 merely ((point (B G) ; x) = (b ; y)) }.
   
@@ -203,7 +218,9 @@ Local Notation B := classifying_space.
 
      todo: use [group_loops]
   *)
-  Definition stabilizer (X : ooAction G) (x : action_space X) : ooGroup.
+  Definition stabilizer
+             (X : ooAction G) (x : action_space X)
+             : ooGroup.
   Proof.
     pose (Ox := Build_pType (sigT (orbit_action X x))
                             ( (point (B G)) ; (x ; tr idpath ) ) ).
@@ -214,28 +231,27 @@ Local Notation B := classifying_space.
     apply BuildIsSurjection; simpl; intros [b ty].
     destruct ty as (y, p).
     strip_truncations. apply tr. exists tt.
-    refine (path_sigma _ _ _ _ _).
-    apply path_sigma_hprop. simpl.
-      Unshelve. Focus 2. exact (p ..1).
-    refine ((the_same _ _ _ _) @ _).
-    exact (p ..2).
+    simple refine (path_sigma _ _ _ _ _).
+      - exact (p ..1).
+      - apply path_sigma_hprop. simpl.
+        refine ((transport_on_first_coordinate _ _ _ _) @ _).
+        exact (p ..2).
   Defined.
  
   (* map from stabilizer to group *)
   Definition stabilizer_to_G (X : ooAction G) (x : action_space X) :
                ooGroupHom (stabilizer X x) G.
   Proof.  
-    refine (Build_pMap _ _ _ _).
-    Unshelve. Focus 2.
-      exact pr1.
-      reflexivity.
+    simple refine (Build_pMap _ _ _ _).
+      - exact pr1.
+      - reflexivity.
   Defined.
   
   
   (* how it computes on paths *)
   Definition stabilizer_to_G_path
-               (X : ooAction G) (x : action_space X) (l : stabilizer X x) :
-                 (stabilizer_to_G X x) l = l ..1.
+             (X : ooAction G) (x : action_space X) (l : stabilizer X x)
+             : (stabilizer_to_G X x) l = l ..1.
   Proof.
     unfold stabilizer_to_G. unfold grouphom_fun. unfold loops_functor. simpl.
     refine (concat_1p _ @ _). refine (concat_p1 _ @ _).
@@ -245,17 +261,22 @@ Local Notation B := classifying_space.
   
   (* the stabilizer of [x] acts trivially on [x] *)
   Definition stab_acts_trivially
-               {X : ooAction G} {x : action_space X} (l : stabilizer X x) :
-                 transport X ((stabilizer_to_G X x) l) x = x.
+             {X : ooAction G} {x : action_space X} (l : stabilizer X x)
+             : transport X ((stabilizer_to_G X x) l) x = x.
   Proof.
-    pose (pr1l2 := pr1_path l..2). pose (pr := (the_same _ _ _ _)^ @ pr1l2). simpl in pr.
-    rewrite (stabilizer_to_G_path _ _ l). apply pr.
+    pose (pr1l2 := pr1_path l..2).
+    pose (pr := (transport_on_first_coordinate _ _ _ _)^ @ pr1l2).
+    simpl in pr.
+    refine (ap (fun path => transport X path x)
+               (stabilizer_to_G_path _ _ l) @ _).
+    apply pr.
   Defined.
   
   
   (* relation of being in the same coset *)
-  Definition in_coset_morph {H : ooGroup} (f : ooGroupHom H G) (b : B G)
-  : (point (B G)) = b -> (point (B G)) = b -> Type
+  Definition in_coset_morph
+             {H : ooGroup} (f : ooGroupHom H G) (b : B G)
+             : (point (B G)) = b -> (point (B G)) = b -> Type
     := fun g1 g2 => hfiber f (g1 @ g2^).
   
   
@@ -265,32 +286,38 @@ Local Notation B := classifying_space.
   
   
   (* action on cosets *)
-  Definition action_cosets {H : ooGroup} (f : ooGroupHom H G) : ooAction G
+  Definition action_cosets
+             {H : ooGroup} (f : ooGroupHom H G)
+             : ooAction G
   := fun b => reltocoeq (in_coset_morph f b).
   
   
   (* fiberwise map cosets action to orbit action *)
-  Definition cosets_to_orbit {X : ooAction G} {x : action_space X} (b : B G) :
-                             (action_cosets (stabilizer_to_G X x) b) -> orbit_action X x b.
+  Definition cosets_to_orbit
+             {X : ooAction G} {x : action_space X} (b : B G)
+             : (action_cosets (stabilizer_to_G X x) b) -> orbit_action X x b.
   Proof.
-    refine (Coeq_rec _ _ _).
-    Unshelve. Focus 2.
-      intro p. exists (p # x).
-    apply tr. refine (path_sigma _ _ _ _ _).
-    Unshelve. Focus 2. simpl. exact p. reflexivity.
-      intros. simpl.
-      refine (path_sigma _ _ _ _ _).
-    Unshelve. Focus 2. simpl. induction b0 as (p, pp).
-    apply moveL_transport_p. rewrite <- transport_pp.
-    induction pp as (l, t).
-    refine ( (ap (fun e => transport X e x) t^) @ _ ).
-    pose (l1 := l ..1).
-    pose (l2 := l ..2).
-    pose (l21 := ap pr1 l2).
-    pose (pr := (the_same _ _ _ _)^ @ l21). simpl in pr.
-    rewrite (stabilizer_to_G_path _ _ l). apply pr.
-    simpl.
-    apply path_ishprop.
+    simple refine (Coeq_rec _ _ _).
+      - intro p. exists (p # x).
+        apply tr. simple refine (path_sigma _ _ _ _ _).
+          + simpl. exact p.
+          + reflexivity.
+      - intros. simpl.
+        simple refine (path_sigma _ _ _ _ _).
+          + simpl. induction b0 as (p, pp).
+            apply moveL_transport_p. rewrite <- transport_pp.
+            induction pp as (l, t).
+            refine ( (ap (fun e => transport X e x) t^) @ _ ).
+            pose (l1 := l ..1).
+            pose (l2 := l ..2).
+            pose (l21 := ap pr1 l2).
+            pose (pr := (transport_on_first_coordinate _ _ _ _)^ @ l21).
+            simpl in pr.
+
+            refine (ap (fun path => transport X path x)
+                       (stabilizer_to_G_path _ _ l) @ _).
+            apply pr.
+          + apply path_ishprop.
   Defined.
   
   (* this is probably false but it seems to be true for
@@ -302,25 +329,28 @@ Local Notation B := classifying_space.
   Proof.
   *)
   
+End MoreDefinitions.
 
 
-
-
-
+Section More.
 
 (* other *)
   (* stabilizer: *)
-  Definition stabilizer' (X : ooAction G) (x : action_space X) : ooGroup
+  Definition stabilizer'
+             (X : ooAction G) (x : action_space X)
+             : ooGroup
   := group_loops (Build_pType (sigT (orbit_action X x))
                               ( (point (B G)) ; (x ; tr idpath ) ) ).
   
   (* map from stabilizer' to group *)
-  Definition stabilizer'_to_G (X : ooAction G) (x : action_space X) :
-               ooGroupHom (stabilizer' X x) G.
+  Definition stabilizer'_to_G
+             (X : ooAction G) (x : action_space X)
+             : ooGroupHom (stabilizer' X x) G.
   Proof.
-    refine (Build_pMap _ _ _ _).
-    Unshelve. Focus 2.
-      intro bb.
-      exact (pr1 (pr1 bb)).
-    reflexivity.
+    simple refine (Build_pMap _ _ _ _).
+      - intro bb.
+        exact (pr1 (pr1 bb)).
+      - reflexivity.
   Defined.
+
+End More.
